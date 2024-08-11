725c1af30811547d2af155df05d440e1d5a24f69
SchreiberMarcio <contaasgard10@gmail.com>
2024-08-11 16:50:08 -0300

    atualizações

--- src/classes/Product.ts
+++ src/classes/Product.ts
@@ -1,29 +1,68 @@
 import { Base } from "./Base";
+import { Rate, Rating } from "./Rating";
+import { Comment } from "./Comment";
+import { ratings } from "./../db/ratings";
+import { comments } from "./../db/comment";
+import { User } from "./User";
 
-export class Product extends Base{
+export class Product extends Base {
+  public name: string;
+  public value: number;
+  private _rating: Rating[];
+    id: any;
 
-    public name:string;
-    public value:number;
+  constructor(name: string, value: number) {
+    super();
+    this.name = name;
+    this.value = value;
+    this._rating = [];
+  }
 
-    constructor(name:string, value:number) {
-        super();
-        this.name = name;
-        this.value = value;
-    };
+  public show(): string {
+    const resposta = `Produto: ${this.name}\nValor: R$${this.value}`;
+    console.log(resposta);
+    console.log(`\n---------------------\n`);
+    return resposta;
+  }
 
-    public show():string{
-        return""
+  public showDetails(): string {
+    const productComments = comments.filter(
+      (comment) => comment.product === this
+    );
+
+    if (productComments.length !== 0) {
+      productComments.forEach((comment) =>
+        console.log(`[${comment.fromUser.username}]: Nota: (${comment.content})`)
+      
+      );
+    } else {
+      console.log("Não existem comentários!");
     }
-    
-    private showDetails():string{
-        return""
+    console.log(`\n---------------------\n`);
+
+    const productRatings = ratings.filter((rating) => rating.product === this);
+
+    if (productRatings.length !== 0) {
+      const averageRating =
+        productRatings.reduce((sum, rating) => sum + rating.rate, 0) /
+        productRatings.length;
+      console.log(`A média das avaliações é: ${averageRating.toFixed(1)}`);
+    } else {
+      console.log("Não existem avaliações!");
     }
 
-                                // user = User 
-    public comment(content:string, user: any):void{
-    }
+    return ``;
+  }
 
-                            // user = User 
-    public rate(rate:number, user:any):void{  
-    }
+  // user = User
+  public comment(content: string, user: any): void {
+    const comment = new Comment(content, this, user);
+    comments.push(comment);
+  }
+
+  // user = User
+  public rate(rate: Rate, user: User): void {
+    const rating = new Rating(rate, this, user);
+    ratings.push(rating);
+  }
 }
--- src/classes/User.ts
+++ src/classes/User.ts
@@ -1,30 +1,34 @@
 import { Base } from "./Base";
 import { Product } from "./Product";
+import { Rate } from "./Rating";
 
 export class User extends Base {
-    public name : string;
-    public username: string;
-    public email: string;
-    private _cart: Product[]; 
+  public name: string;
+  public username: string;
+  public email: string;
+  private _cart: Product[];
 
-    constructor (name: string , username: string , email: string) {
-        super ();
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this._cart = [];
-    }
+  constructor(name: string, username: string, email: string) {
+    super();
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this._cart = [];
+  }
 
-    public addToCart (product:Product): void { 
+  public addToCart(product: Product): void {
+    this._cart.push(product);
+  }
 
-    }
+  public removeFromCart(product: Product): void {
+    this._cart = this._cart.filter((p) => p.id !== product.id);
+  }
 
-    public removeFromCart (product:Product): void {
+  public showProducts(): string {
+    return this._cart.map((product) => product.show()).join(", ");
+  }
 
-    }
-
-    public showProducts (): string {
-        return "";
-    }
-
+  public rateProduct(product: Product | null, rate: Rate): void {
+   
+  }
 }
